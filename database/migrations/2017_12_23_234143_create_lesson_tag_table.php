a56fd9ec03d87c98004ec7445ac10641256460ec
kinyou <kinyou_xy@126.com>
2017-12-24 00:11:47 +0800

    增加tags和lesson表

---
+++ database/migrations/2017_12_23_234143_create_lesson_tag_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesson_tag', function (Blueprint $table) {
+            $table->increments('id');
+            //建立lesson_id索引
+            $table->integer('lesson_id')->unsigned()->index();
+            //建立外键,包括关联删除
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+
+            //建立tag_id索引
+            $table->integer('tag_id')->unsigned()->index();
+            //建立外键,包括关联删除
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesson_tag');
+    }
+}
